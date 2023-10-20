0fa1b17f3316767a28ee035b160cae41b6c05f34
JamDev0 <jamdeveloper0@gmail.com>
2023-10-20 17:27:36 -0300

    feat: base theme config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,33 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Chakra_Petch, Crimson_Text, Inter, Rubik_Mono_One } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+})
+ 
+const crimsonText = Crimson_Text({
+  subsets: ['latin'],
+  variable: '--font-crimson-text',
+  display: 'swap',
+  weight: '400'
+})
+
+const rubikMonoOne = Rubik_Mono_One({
+  subsets: ['latin'],
+  variable: '--font-rubik-mono-one',
+  display: 'swap',
+  weight: '400'
+})
+
+const chakraPetch = Chakra_Petch({
+  subsets: ['latin'],
+  variable: '--font-chakra-petch',
+  display: 'swap',
+  weight: ['500', '400']
+})
 
 export const metadata: Metadata = {
   title: 'Juan Garcia',
@@ -16,7 +41,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${crimsonText.variable} ${rubikMonoOne.variable} ${chakraPetch.variable}`}>{children}</body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,71 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          '50': '#ECF6FD',
+          '100': '#B5DCF7',
+          '200': '#7EC1F1',
+          '300': '#47A7EB',
+          '400': '#188BDC',
+          '500': '#1268A5',
+          '600': '#0A3A5C',
+          '700': '#062337',
+          '800': '#041825',
+          '900': '#020C12',
+        },
+
+        secondary: {
+          '50': '#FEF0EB',
+          '100': '#FBC5B1',
+          '200': '#F89A77',
+          '300': '#F56E3D',
+          '400': '#E9470C',
+          '500': '#AE3509',
+          '600': '#742306',
+          '700': '#3A1203',
+          '800': '#270C02',
+          '900': '#130601',
+        },
+
+        tertiary: {
+          '50': '#ECFEEC',
+          '100': '#B1FBB1',
+          '200': '#77F877',
+          '300': '#3DF53D',
+          '400': '#0CE90C',
+          '500': '#09AE09',
+          '600': '#067406',
+          '700': '#033A03',
+          '800': '#022702',
+          '900': '#011301',
+        },
+
+        quaternary: {
+          '400': '#E935C1',
+        },
+
+        background: '#0D0F10',
+
+        text: {
+          DEFAULT: '#E7ECF1',
+          darken: '#9CA6AF',
+          component: '#111011',
+        },
+
+        container: {
+          '700': '#2D3439',
+          '800': '#1B1F22',
+          '900': '#15181A',
+        }
       },
+
+      fontFamily: {
+        'inter': 'var(--font-inter)',
+        'crimson': 'var(--font-crimson-text)',
+        'rubik': 'var(--font-rubik-mono-one)',
+        'chakra': 'var(--font-chakra-petch)',
+      }
     },
   },
   plugins: [],
