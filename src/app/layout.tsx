0bc740b7d5cac842817a7153f47b6ee213d7c371
JamDev0 <jamdeveloper0@gmail.com>
2023-10-20 23:36:08 -0300

    feat: apps base layout structure at root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${fonts}`}>
+      <body className={`${fonts} flex flex-col items-stretch min-h-screen`}>
         {children}
       </body>
     </html>
