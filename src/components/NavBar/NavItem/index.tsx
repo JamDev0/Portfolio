40e8eb7df2c7c8a30d44ed6948e4e7b008d60198
JamDev0 <jamdeveloper0@gmail.com>
2023-10-25 13:35:32 -0300

    fix: NavItem component was using old Typography component

--- src/components/NavBar/NavItem/index.tsx
+++ src/components/NavBar/NavItem/index.tsx
@@ -1,4 +1,3 @@
-import { Typography } from "@/components/Typography";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -15,9 +14,9 @@ export function NavItem({ children, href }: NavItemProps) {
   return (
     <Link href={href} className="md:border-r-2 md:py-2 md:border-container-700 md:last:border-r-0">
       <li className="flex justify-center py-3 px-4 list-none border-b-[3px] border-container-900 md:border-b-0 md:py-0">
-          <Typography data-active={isCurrentPage} as="span" type='heading-3' className="px-3 border-b border-primary-400 md:border-b-0 md:data-[active=true]:border-b">
+          <span data-active={isCurrentPage} className="px-3 heading-3 border-b border-primary-400 md:border-b-0 md:data-[active=true]:border-b">
             {children}
-          </Typography>
+          </span>
       </li>
     </Link>
   )
